702ed0d26e8518fcb395a6c5a092453867086a82
Andrew Dong <andrewdong1994@gmail.com>
2019-03-27 18:04:09 -0400

    Add story component

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -4,7 +4,6 @@ export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER'
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER'
 export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS'
 
-
 export const receiveCurrentUser = user => {
     return {
         type: RECEIVE_CURRENT_USER,
@@ -43,4 +42,4 @@ export const signup = user => dispatch => {
         user => dispatch(receiveCurrentUser(user)),
         err => dispatch(receiveSessionErrors(err))
     )
-} 
+}
---
+++ frontend/components/header/nav.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import LogoutNav from './logout_nav'
+import LoginNav from './login_nav'
+
+const Nav = ({ currentUser }) => {
+    return currentUser ? <LogoutNav /> : <LoginNav />
+}
+
+const mapStateToProps = state => {
+    const currentUser = typeof state.session.id === 'number'
+    return {
+        currentUser
+    }
+}
+
+export default connect(
+    mapStateToProps
+)(Nav)
