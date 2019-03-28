b9fb0db9a565e9aa769e7909f496118de8188cf8
Andrew Dong <andrewdong1994@gmail.com>
2019-03-28 11:24:54 -0400

    Add Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,59 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.destroy_all
+Story.destroy_all
+Following.destroy_all
+
+andrew = User.new(
+  name: 'Andrew Dong',
+  bio: 'Software Engineer, lover of sports',
+  email: 'andrew@gmail.com',
+  password: 'testing'
+)
+andrew.avatar.attach(io: File.open('app/assets/images/andrew.jpg'), filename: 'andrew.jpg')
+andrew.save
+
+amanda = User.new(
+  name: 'Amanda Flink',
+  bio: 'Fitness Instructor, aspiring coder',
+  email: 'amanda@gmail.com',
+  password: 'testing'
+)
+amanda.avatar.attach(io: File.open('app/assets/images/amanda.jpg'), filename: 'amanda.jpg')
+amanda.save
+
+matthew = User.new(
+  name: 'Matthew Jay',
+  bio: 'Accounting Professor, general baller',
+  email: 'matthew@gmail.com',
+  password: 'testing'
+)
+matthew.avatar.attach(io: File.open('app/assets/images/matthew.jpg'), filename: 'matthew.jpg')
+matthew.save
+
+stacy = User.new(
+  name: 'Stacy Bueno',
+  bio: 'Investment Banker, fun lover',
+  email: 'stacy@gmail.com',
+  password: 'testing'
+)
+stacy.avatar.attach(io: File.open('app/assets/images/stacy.jpg'), filename: 'stacy.jpg')
+stacy.save
+
+theo = User.new(
+  name: 'Theo Beers',
+  bio: 'PhD Candidate in Egyptian Studies',
+  email: 'theo@gmail.com',
+  password: 'testing'
+)
+theo.avatar.attach(io: File.open('app/assets/images/theo.jpg'), filename: 'theo.jpg')
+theo.save
+
+mike = User.new(
+  name: 'Big Mike',
+  bio: "'Flag football director, lover of song",
+  email: 'mike@gmail.com',
+  password: 'testing'
+)
+mike.avatar.attach(io: File.open('app/assets/images/mike.jpg'), filename: 'mike.jpg')
+mike.save 
