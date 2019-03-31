3cc1a865e431f86b7dee8d705839527ef4241a83
Andrew Dong <andrewdong1994@gmail.com>
2019-03-31 12:22:04 -0400

    Allow login signup switch:

--- frontend/components/modal/modal.jsx
+++ frontend/components/modal/modal.jsx
@@ -2,24 +2,27 @@ import React from 'react'
 import ModalLoginForm from './modal_login_form'
 import ModalSignupForm from './modal_signup_form'
 
-const Modal = ({reveal, errors, login, signup, hide}) => {
+const Modal = ({
+  reveal, errors, login, signup, hide, revealModalSignup, revealModalLogin }) => {
 
   const classes = reveal ? 'modal' : 'modal hide'
   let modal = <div></div>
 
   if (reveal === 'login') {
     modal = <ModalLoginForm
-              title="Log In"
-              errors={errors}
-              submitAction={login}
-              hide={hide} />
+      title="Log In"
+      errors={errors}
+      submitAction={login}
+      hide={hide}
+      revealModalSignup={revealModalSignup} />
 
   } else if (reveal) {
     modal = <ModalSignupForm
-              title="Sign Up"
-              errors={errors}
-              submitAction={signup}
-              hide={hide} />
+      title="Sign Up"
+      errors={errors}
+      submitAction={signup}
+      hide={hide}
+      revealModalLogin={revealModalLogin} />
   }
 
   return (
