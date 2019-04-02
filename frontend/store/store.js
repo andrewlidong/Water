2821d383d8f8d715d19b6ab8eb3e6e21ac9bc454
Andrew Dong <andrewdong1994@gmail.com>
2019-04-02 17:18:43 -0400

    Updated Store

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,16 +1,16 @@
-import { createStore, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
-import logger from 'redux-logger'
-import rootReducer from '../reducers/root_reducer'
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import rootReducer from '../reducers/root_reducer';
 
-const middlewares = [thunk]
+const middlewares = [thunk];
 
 if (process.env.NODE_ENV === `development`) {
-  middlewares.push(logger)
+  middlewares.push(logger);
 }
 
-export default (defaultState) => {
+export default defaultState => {
   return createStore(
     rootReducer, defaultState, applyMiddleware(...middlewares)
   )
-}
+};
